ae766fa056c385295c496b2994569b01f9e42944
ThiagoHDMiranda <106932990+ThiagoHDMiranda@users.noreply.github.com>
2024-06-04 17:22:02 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,9 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
-  // console.log({
-  //   host: process.env.POSTGRES_HOST,
-  //   user: process.env.POSTGRES_USER,
-  //   port: process.env.POSTGRES_PORT,
-  //   database: process.env.POSTGRES_DB,
-  //   password: process.env.POSTGRES_PASSWORD,
-  // });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -31,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : trueS;
+}
