48adf41cde56fb8922b4e98c8a60f6f84bcd31c6
ThiagoHDMiranda <106932990+ThiagoHDMiranda@users.noreply.github.com>
2026-05-25 23:09:45 +0000

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,22 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ message, action }) {
+    super(message || "Não foi possível encontrar este recurso no sistema.");
+    this.name = "NotFoundError";
+    this.action =
+      action || "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
