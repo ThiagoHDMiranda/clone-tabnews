68e688599bc082cbe1a1511f67cc996e81a27804
ThiagoHDMiranda <106932990+ThiagoHDMiranda@users.noreply.github.com>
2026-05-25 23:14:04 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/user/[username]` endpoint

---
+++ pages/api/v1/users/[username]/index.js
@@ -0,0 +1,15 @@
+import controller from "infra/controller";
+import user from "models/user";
+import { createRouter } from "next-connect";
+
+const router = createRouter();
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const { username } = request.query;
+  const userFound = await user.findOneByUsername(username);
+
+  return response.status(200).json(userFound);
+}
